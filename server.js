afa7d978e062d959fd26a5a2fa8648c43bed040b
Angel Angelov <angel.s.angelov@gmail.com>
2018-12-06 17:44:52 +0200

    initial

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var server = require('http').Server;
+var app = new express(server);
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || '3000', er => {
+  console.log('Server started ...');
+});
