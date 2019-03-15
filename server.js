05c30dcee9f0612b47e40c0b2e0a72b62cf27351
Angel Angelov <angel.s.angelov@gmail.com>
2019-03-15 12:24:44 +0200

    babel

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res) => {
+  console.log('file served');
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
